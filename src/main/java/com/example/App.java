a4818e860300308d52a94062c75ecf940295996c
Manoel Campos <manoelcampos@gmail.com>
2023-06-05 15:26:00 -0300

    Adiciona print no projeto de exemplo

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,8 @@ package com.example;
 public class App {
     
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Aplicação Java de Exemplo\n");
     }
     
 }
