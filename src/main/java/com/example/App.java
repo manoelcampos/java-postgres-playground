f4b54ec1f8212a5b1263369c087c52ed012fd3c6
Manoel Campos <manoelcampos@gmail.com>
2023-06-05 16:22:44 -0300

    Adicionar driver JDBC do postgresql e mostra exemplo de consulta

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,29 @@
 package com.example;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println();
         System.out.println("Aplicação Java de Exemplo\n");
+        listarEstados();
+    }
+
+    public static void listarEstados()  {
+        System.out.println("Listando estados cadastrados no banco de dados");
+        try {
+            Class.forName("org.postgresql.Driver");
+            var conn = DriverManager.getConnection("jdbc:postgresql://localhost/meubanco", "postgres", "");
+            var stm = conn.createStatement();
+            var result = stm.executeQuery("select * from estado");
+            while(result.next()) {
+                System.out.println(result.getString("nome"));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
     
 }
