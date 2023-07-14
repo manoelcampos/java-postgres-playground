6566fe6e0ec12498efbd016b5c9ae30c57c24f9e
Manoel Campos da Silva Filho <manoelcampos@gmail.com>
2023-07-14 18:18:12 +0000

    Refatora código de consulta de estados

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     public static void main(String[] args){
@@ -14,18 +15,23 @@ public class App {
         System.out.println("Listando estados cadastrados no banco de dados");
         try {
             Class.forName("org.postgresql.Driver");
-            try(var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "")){
-                var stm = conn.createStatement();
-                var result = stm.executeQuery("select * from estado");
-                while(result.next()) {
-                    System.out.println(result.getString("nome"));
-                }
-            }
         } catch (ClassNotFoundException e) {
-            System.out.println("Não foi possível carregar o driver JDBC para acesso ao banco de dados: " + e.getMessage());
-        } catch (SQLException e) {
-            System.out.println("Não foi executar a consulta ao banco de dados: " + e.getMessage());
+            System.err.println("Não foi possível carregar a biblioteca para acesso ao banco de dados: " + e.getMessage());
         }
-    }
+
+        Statement statement = null;
+        try(var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "")){
+            System.out.println("Conexão com o banco realizada com sucesso.");
+
+            statement = conn.createStatement();
+            var result = statement.executeQuery("select * from estado");
+            while(result.next()){
+                System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            }
+        } catch (SQLException e) {
+            if(statement == null)
+                System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
+            else System.err.println("Não foi possível executar a consulta ao banco: " + e.getMessage());
+        }    }
     
 }
