dbf9a966b4364810dc1c04fe23394327a5369963
Guilherme Carlos <guilherme.fpedrinha@gmail.com>
2023-07-31 18:55:35 +0000

    Aulas de tipos primitivos

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,19 @@ public class App {
     public static void main(String[] args) {
         System.out.println();
         System.out.println();
-        System.out.println(x:"minha aplicacao java");
+        System.out.println("\n ################## INICIO ########################################  \n ");
+
+        int idadeInt = 10;
+        double pesoInt = 80.5;
+
+        char sexo = 'M';
+        boolean necessidadesEspeciais = false;
+
+
+        System.out.println("Idade e o Peso do meliante é: " + idadeInt + " e " + pesoInt + " respectivamente " + "\n");   
+        System.out.println("Tem necessidades especiais: " + necessidadesEspeciais +"\n");
+
+
+        System.out.println("\n ################## FIM ###########################################  \n");
     }
 }
